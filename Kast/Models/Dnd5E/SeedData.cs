a6aee8e16403c31ba3d718631ff8ef3fa7050e2d
Kasper steenstrup <Kasper.steenstrup@3shape.com>
2018-03-09 16:34:54 +0100

    added see data

---
+++ Kast/Models/Dnd5E/SeedData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Kast.Models.Dnd5E
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new Dnd5EContext(
+                serviceProvider.GetRequiredService<DbContextOptions<Dnd5EContext>>()))
+            {
+                // Look for any Spell.
+                if (context.Spells.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                var spells = SpellBuilder.DefualdSpellBuilder().LoadCantrip().LoadLvl1Spell().LoadLvl2Spell().Build().ToList();
+                
+                context.Spells.AddRange(spells);
+                context.SaveChanges();
+            }
+        }
+
+    }
+}
--- Kast/Program.cs
+++ Kast/Program.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore;
+﻿using System;
+using Kast.Models.Dnd5E;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace Kast
 {
@@ -7,7 +11,25 @@ namespace Kast
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    // Requires using MvcMovie.Models;
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
